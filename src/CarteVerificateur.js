b905746f49e0e89f91647e089888d8c9dfc5f426
Kabore <dkabore45@gmail.com>
2019-03-26 23:44:56 +0000

    Implementation de l'algorithme de Luhn

--- src/CarteVerificateur.js
+++ src/CarteVerificateur.js
@@ -1,7 +1,6 @@
 
 //ContextBanque permet de recuperer les valeurs de retour de chaques types de cartes (visa, master, americaexpress)
 
-
 class ContextBanque {
     constructor(type){
         switch(type) {
@@ -34,20 +33,34 @@ class Strategy {
 
 class StrategyVisaCarte extends Strategy{
 
-    InterfaceCarteVerif (numCarte){
-    		
-        	var taille = numCarte.length;
-    		var deuxL = numCarte.substring(0, 2);
-    		if (Number(numCarte)){
-    			if (taille == "15" && (deuxL == "14" || deuxL == "15")){
-    				console.log("Carte Visa");
-    			} else {
-    				console.log("ce n'est pas une carte Visa");
-    				}
-    		}else {
-    			console.log ("Le numero de la carte n'est pas valide");
-    		}
-       }	
+	 InterfaceCarteVerif (numCarte){
+	    	var nSomme = 0, nInt = 0, nPaire = false;    	
+	        	var taille = numCarte.length;
+	    		var deuxL = numCarte.substring(0, 2);
+	    		if (Number(numCarte)){
+	    			for (var n = taille - 1; n >= 0; n--) {
+	    				var nInt = numCarte.charAt(n),
+	    				nInt = parseInt(nInt, 10);
+	    				if (nPaire) {
+	    					if ((nInt *= 2) > 9) nInt -= 9;
+	    				}
+	    				nSomme += nInt;
+	    				nPaire = !nPaire;	  
+	    			} 
+	    			
+	    			if ((nSomme % 10) == 0){
+	    				if (taille == "15" && (deuxL == "14" || deuxL == "15")){
+	    					console.log("Carte Visa");
+	    				} else {
+	    					console.log("ce n'est pas une carte Visa");
+	    				} 
+	    			} else {
+	    				console.log("le numero ne corresponds pas à un numero de carte de credit");
+	    			}
+	    		}else {
+	    			console.log ("carte non valide(les lettres et les caratères speciaux ne sont pas pris en charge)");
+	    		}
+	       }			
 }
 
 
@@ -55,39 +68,65 @@ class StrategyVisaCarte extends Strategy{
 
 class StrategyMasterCarte extends Strategy{
 
-    InterfaceCarteVerif (numCarte){
-    		
-        	var taille = numCarte.length;
-    		var deuxL = numCarte.substring(0, 2);
-    		if (Number(numCarte)){
-    			if (taille == "16" && (deuxL == "41" || deuxL == "40")){
-    				console.log("Carte Master");
-    			} else {
-    				console.log("ce n'est pas une carte Master");
-    				}
-    		}else {
-    			console.log ("Le numero de la carte n'est pas valide");
-    		}
-       }	
+	  InterfaceCarteVerif (numCarte){
+	    	var nSomme = 0, nInt = 0, nPaire = false;    	
+	        	var taille = numCarte.length;
+	    		var deuxL = numCarte.substring(0, 2);
+	    		if (Number(numCarte)){
+	    			for (var n = taille - 1; n >= 0; n--) {
+	    				var nInt = numCarte.charAt(n),
+	    				nInt = parseInt(nInt, 10);
+	    				if (nPaire) {
+	    					if ((nInt *= 2) > 9) nInt -= 9;
+	    				}
+	    				nSomme += nInt;
+	    				nPaire = !nPaire;	    				
+	    			} 
+	    			
+	    			if ((nSomme % 10) == 0){
+	    				if (taille == "16" && (deuxL == "40" || deuxL == "41")){
+	    					console.log("Carte Master");
+	    				} else {
+	    					console.log("ce n'est pas une carte Master");
+	    				} 
+	    			} else {
+	    				console.log("le numero ne corresponds pas à un numero de carte de credit");
+	    			}
+	    		}else {
+	    			console.log ("carte non valide(les lettres et les caratères speciaux ne sont pas pris en charge)");
+	    		}
+	       }		
 }
 
 // Algorithme de verification de la carte AmericaExpress ( implementation de l'interface)
 
 class StrategyAmericaExpressCarte extends Strategy{
 
-
     InterfaceCarteVerif (numCarte){
-    		
+    	var nSomme = 0, nInt = 0, nPaire = false;   	
         	var taille = numCarte.length;
     		var deuxL = numCarte.substring(0, 2);
     		if (Number(numCarte)){
-    			if (taille == "17" && (deuxL == "35" || deuxL == "30")){
-    				console.log("Carte AmericaExpress");
-    			} else {
-    				console.log("ce n'est pas une carte AmericaExpress");
+    			for (var n = taille - 1; n >= 0; n--) {
+    				var nInt = numCarte.charAt(n),
+    				nInt = parseInt(nInt, 10);
+    				if (nPaire) {
+    					if ((nInt *= 2) > 9) nInt -= 9;
     				}
+    				nSomme += nInt;
+    				nPaire = !nPaire;
+    			}    			
+    			if ((nSomme % 10) == 0){
+    				if (taille == "17" && (deuxL == "35" || deuxL == "39")){
+    					console.log("Carte AmericaExpress");
+    				} else {
+    					console.log("ce n'est pas une carte AmericaExpress");
+    				} 
+    			} else {
+    				console.log("le numero ne corresponds pas à un numero de carte de credit");
+    			}
     		}else {
-    			console.log ("Le numero de la carte n'est pas valide");
+    			console.log ("carte non valide(les lettres et les caratères speciaux ne sont pas pris en charge)");
     		}
        }	
 }
@@ -98,11 +137,11 @@ class StrategyAmericaExpressCarte extends Strategy{
 
 function Client() {
     let VisaTest = new ContextBanque("Visa")
-    VisaTest.ContextInterface("142551111111111")
+    VisaTest.ContextInterface("142551111111101")
     let MasterTest = new ContextBanque("Master")
-    MasterTest.ContextInterface("4011111111111111")
+    MasterTest.ContextInterface("4104885788648930")
     let AmericaExpressTest = new ContextBanque("AmericaExpress")
-    AmericaExpressTest.ContextInterface("35522121421221214")
+    AmericaExpressTest.ContextInterface("35522121421221234")
 }
 
 Client()
